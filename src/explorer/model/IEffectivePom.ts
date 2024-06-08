f2ef885977280558cb82fdd257e6625aa443147a
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-08 07:34:03 +0300

    squash!

--- src/explorer/model/IEffectivePom.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-export interface IEffectivePom {
-  pomPath: string;
-  ePomString?: string;
-  ePom?: any;
-}
